426d491ad77984b0e9c995ee399bbba915d0a393
RyanPSN <ryangregoire286@gmail.com>
2024-10-04 15:49:56 -0400

    Add project files.

---
+++ RandomAddition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RandomMathDLL;
+
+namespace RandomAddition
+{
+    class Program
+    {
+
+        public static RandomNum projectName = new RandomNum();
+        
+        static void Main(string[] _)
+        {
+
+            string proName = "Random Math Libary";
+            projectName.ProjectName("Project Name:", proName);
+
+            int number_1 = 10;
+            int number_2 = 1;
+
+            Console.WriteLine();
+            RandomNum Number = new RandomNum();
+
+            int num1 = Number.getRandomNum(number_1, number_2);
+            int num2 = Number.getRandomNum(number_1, number_2);
+                        
+            string op = Number.getOperator();
+
+
+            string result = $"{num1} {op} {num2}";
+            projectName.ProjectName("Problem:", result);
+
+            Console.Write("Op: {0}", op);
+            Console.ReadLine();
+                    
+
+
+        }
+    }
+}
---
+++ RandomMathDLL/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomMathDLL
+{
+    public class RandomNum
+    {
+        public static List<string> opAdd = new List<string>();
+        public static Random r = new Random();
+        public string GetHelloMessage(string message)
+        {
+
+            return message.ToUpper();
+        }
+
+        public void ProjectName(string name, string x1)
+        {
+            Console.WriteLine(name.ToUpper() + " " + x1);
+
+        }
+
+        public string getOperator()
+        {
+
+            opAdd.Add("+");
+            opAdd.Add("-");
+            opAdd.Add("*");
+            opAdd.Add("/");
+            int n = r.Next(opAdd.Count);
+
+            return opAdd[n];
+
+        }
+
+        public int getRandomNum(int a, int b)
+        {
+            return r.Next(b, a);
+        }
+
+    }
+}
